3d9b124b68b0696ebdd9bd5ab41fd757917a5d0d
rgsk <guptarahul70322@gmail.com>
2025-12-16 16:57:51 +0530

    Initial Commit

---
+++ Music Player/Music_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_PlayerApp.swift
+//  Music Player
+//
+//  Created by apple on 16/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Music_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
